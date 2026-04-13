ee608338ff5a2887770cc6bfdb08848fd69d2d0b
kumarbanty143 <himanshupatra702@gmail.com>
2026-04-13 23:27:56 +0530

    build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GitProfile from './components/GitProfile.tsx';
+import GitProfile from './components/gitprofile.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
